957ac199579e5a7d52b19e505fc09f760c6fefc2
felipejsm <felipejsm41@gmail.com>
2024-11-10 23:02:55 -0300

    simple server

---
+++ cmd/server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Print(w, "Hello World Render")
+	})
+	err := http.ListenAndServer(":8080", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
+}
