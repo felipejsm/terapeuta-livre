d0958747ca0fdde095e0b0be97d360fb18b61ebe
felipejsm <felipejsm41@gmail.com>
2024-12-15 18:55:00 -0300

    starting patient service

--- internal/services/patient.go
+++ internal/services/patient.go
@@ -1,5 +1,12 @@
 package services
 
-func GetPatients() {
+import (
+	"felipejsm/tp-admin/internal/dtos"
+	repo "felipejsm/tp-admin/internal/repositories"
+	model "felipejsm/tp-admin/internal/models"
+)
 
+func GetPatientDetail(patientId string) (dtos.PatientDto, error) {
+	var patient model.Patient
+	repo.PatientRepository.DB.
 }
