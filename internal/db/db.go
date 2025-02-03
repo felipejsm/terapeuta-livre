febe9c97114951325e2979339d5b5e1f40eb5896
felipejsm <felipejsm41@gmail.com>
2025-02-02 21:27:52 -0300

    prod test

--- internal/db/db.go
+++ internal/db/db.go
@@ -14,11 +14,24 @@ import (
 
 func InitDB() *gorm.DB {
 	config.LoadEnv()
+    // Lendo as variáveis de ambiente
+    dbHost := os.Getenv("DB_HOST")
+    dbUser := os.Getenv("DB_USER")
+    dbPassword := os.Getenv("DB_PASSWORD")
+    dbName := os.Getenv("DB_NAME")
+    dbPort := os.Getenv("DB_PORT")
+
+    // Construindo a string de conexão
+    connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=America/Sao_Paulo",
+        dbHost, dbUser, dbPassword, dbName, dbPort)
+
+    fmt.Println("String de conexão:", connStr)
+    fmt.Println("DB_NAME: ", os.Getenv("DB_NAME"))
 	fmt.Println("DATABASE_URL:", os.Getenv("DATABASE_URL"))
-	dsn := config.GetEnv("DATABASE_URL", "default")
+	//dsn := config.GetEnv("DATABASE_URL", "default")
 	var err error
 
-	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
 
 	if err != nil {
 		log.Fatalf("[Error] database connection failed: %v", err)
